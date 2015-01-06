20ad721d8e1370fe45cf1271c247964e98645518
Shen Weizhong <swzyocowboy@icloud.com>
2015-01-06 18:06:14 +0800

    updated test cases

--- dev/test.coffee
+++ dev/test.coffee
@@ -18,15 +18,65 @@ cssSeparation = require '../index'
 
 
 
-util.log new cssSeparation().getRules('sample.css')
+# util.log new cssSeparation().getRules_AST('sample.css')
 
-new cssSeparation({beautify: true})._writeFile('sample.css', 'common.style.css')
+# new cssSeparation({beautify: true})._writeFile('sample.css', 'common.style.css')
+
+
 
 
 
 describe 'CSS-SEPARATION', ->
 
-	describe '#method()', ->
+	describe '#getIdxListOfRuleContainCC()', ->
+
+		it 'should return [4, 5, 6]', ->
+
+			_cssSeparation = new cssSeparation()
+
+			_rules = _cssSeparation.getRules_AST 'test/sample.css'
+
+			idxsOfRuleContainCC = _cssSeparation.getIdxListOfRuleContainCC _rules
+
+			assert.deepEqual [4, 5, 6], idxsOfRuleContainCC
+
+			return
+
+	describe '#getDirname()', ->
+
+		it 'should return "./", if the given value is just "sample.css"', ->
+
+			_cssSeparation = new cssSeparation()
+
+			assert.equal './', _cssSeparation.getDirname 'sample.css'
+
+			return
+
+		it 'should return "./dest", if the given value is just "dest/sample.css"', ->
+
+			_cssSeparation = new cssSeparation()
+
+			assert.equal './dest', _cssSeparation.getDirname './dest/sample.css'
+
+			return
+
+		it 'should return "dev/stylesheet", if the given value is just "dev/stylesheet/sample.scss"', ->
+
+			_cssSeparation = new cssSeparation()
+
+			assert.equal 'dev/stylesheet', _cssSeparation.getDirname 'dev/stylesheet/sample.scss'
+
+			return
+
+	describe '#createFile()', ->
+
+		it 'should create a file named "created.blank.file.css" in "dest" folder', ->
+
+			_cssSeparation = new cssSeparation()
+
+			_cssSeparation.createFile('', './dest/created.blank.file.css')
+
+			return
 
 		return
 
--- test/test.js
+++ test/test.js
@@ -7,12 +7,38 @@ util = require('gulp-util');
 
 cssSeparation = require('../index');
 
-util.log(new cssSeparation().getRules('sample.css'));
-
-new cssSeparation({
-  beautify: true
-})._writeFile('sample.css', 'common.style.css');
-
 describe('CSS-SEPARATION', function() {
-  describe('#method()', function() {});
+  describe('#getIdxListOfRuleContainCC()', function() {
+    return it('should return [4, 5, 6]', function() {
+      var idxsOfRuleContainCC, _cssSeparation, _rules;
+      _cssSeparation = new cssSeparation();
+      _rules = _cssSeparation.getRules_AST('test/sample.css');
+      idxsOfRuleContainCC = _cssSeparation.getIdxListOfRuleContainCC(_rules);
+      assert.deepEqual([4, 5, 6], idxsOfRuleContainCC);
+    });
+  });
+  describe('#getDirname()', function() {
+    it('should return "./", if the given value is just "sample.css"', function() {
+      var _cssSeparation;
+      _cssSeparation = new cssSeparation();
+      assert.equal('./', _cssSeparation.getDirname('sample.css'));
+    });
+    it('should return "./dest", if the given value is just "dest/sample.css"', function() {
+      var _cssSeparation;
+      _cssSeparation = new cssSeparation();
+      assert.equal('./dest', _cssSeparation.getDirname('./dest/sample.css'));
+    });
+    return it('should return "dev/stylesheet", if the given value is just "dev/stylesheet/sample.scss"', function() {
+      var _cssSeparation;
+      _cssSeparation = new cssSeparation();
+      assert.equal('dev/stylesheet', _cssSeparation.getDirname('dev/stylesheet/sample.scss'));
+    });
+  });
+  describe('#createFile()', function() {
+    it('should create a file named "created.blank.file.css" in "dest" folder', function() {
+      var _cssSeparation;
+      _cssSeparation = new cssSeparation();
+      _cssSeparation.createFile('', './dest/created.blank.file.css');
+    });
+  });
 });
