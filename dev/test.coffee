8121ab328787b2b98f3ba758dcf83283264ad255
Shen Weizhong <swzyocowboy@icloud.com>
2015-01-08 00:46:59 +0800

    added some test cases

--- dev/test.coffee
+++ dev/test.coffee
@@ -10,11 +10,25 @@
 
 
 
-assert        = require 'assert'
+assert         = require 'assert'
 
-util          = require 'gulp-util'
+path           = require 'path'
 
-cssSeparation = require '../index'
+glob           = require 'glob'
+
+util           = require 'gulp-util'
+
+cssSeparation  = require '../index'
+
+_cssSeparation = new cssSeparation
+
+	mute: true
+
+	conditionalClass: ['.ie6', '.ie7', '.ie8', '.ie9', '.ie10', '.ie11']
+
+	beautify: true
+
+	filterCommonCSS: true
 
 
 
@@ -28,25 +42,77 @@ cssSeparation = require '../index'
 
 describe 'CSS-SEPARATION', ->
 
-	describe '#getIdxListOfRuleContainCC()', ->
+	describe '#deal()', ->
 
-		it 'should return [4, 5, 6]', ->
+		it 'should create some file like "sample.ie6.css" around "sample.css" file', ->
 
-			_cssSeparation = new cssSeparation()
+			_cssSeparation.deal 'test/sample.css'
+
+			return
+
+		return
+
+	describe '#getIdxListOfCommonCSS_AST()', ->
+
+		it 'should return [1, 2, 3]', ->
 
 			_rules = _cssSeparation.getRules_AST 'test/sample.css'
 
-			idxsOfRuleContainCC = _cssSeparation.getIdxListOfRuleContainCC _rules
+			idxListOfCommonCSS = _cssSeparation.getPositionsOfSameClassOfRules 'common', _rules
+
+			assert.deepEqual [1, 2, 3], idxListOfCommonCSS
+
+			return
+
+		return
+
+	describe '#getIdxListOfConditialCSS_AST()', ->
+
+		it 'should return [4, 5, 6]', ->
+
+			_rules = _cssSeparation.getRules_AST 'test/sample.css'
+
+			idxsOfRuleContainCC = _cssSeparation.getPositionsOfSameClassOfRules 'condition', _rules
 
 			assert.deepEqual [4, 5, 6], idxsOfRuleContainCC
 
 			return
 
+		return
+
+	describe '#getIdxListOfMediaCSS_AST()', ->
+
+		it 'should return [7]', ->
+
+			_rules = _cssSeparation.getRules_AST 'test/sample.css'
+
+			idxListOfMediaCSS = _cssSeparation.getPositionsOfSameClassOfRules 'media', _rules
+
+			assert.deepEqual [7], idxListOfMediaCSS
+
+			return
+
+		return
+
+	describe '#isContainConditionalSelector()', ->
+
+		it 'should return true if the given selectors list is [".ie7 .frmRegister > .fieldArea"]', ->
+
+			assert.equal true, _cssSeparation.isContainConditionalSelector [".ie7 .frmRegister > .fieldArea"]
+
+			return
+
+		it 'should return true if the given selectors list is [".ie7 .frmRegister > .iptPhoneCodeArea", ".ie8 .frmRegister > .iptPhoneCodeArea", ".ie9 .frmRegister > .iptPhoneCodeArea"]', ->
+
+			assert.equal true, _cssSeparation.isContainConditionalSelector [".ie7 .frmRegister > .iptPhoneCodeArea", ".ie8 .frmRegister > .iptPhoneCodeArea", ".ie9 .frmRegister > .iptPhoneCodeArea"]
+
+			return
+
+		return
+
 	describe '#getDirname()', ->
 
 		it 'should return "./", if the given value is just "sample.css"', ->
-
-			_cssSeparation = new cssSeparation()
 
 			assert.equal './', _cssSeparation.getDirname 'sample.css'
 
@@ -54,25 +120,21 @@ describe 'CSS-SEPARATION', ->
 
 		it 'should return "./dest", if the given value is just "dest/sample.css"', ->
 
-			_cssSeparation = new cssSeparation()
-
 			assert.equal './dest', _cssSeparation.getDirname './dest/sample.css'
 
 			return
 
 		it 'should return "dev/stylesheet", if the given value is just "dev/stylesheet/sample.scss"', ->
 
-			_cssSeparation = new cssSeparation()
-
 			assert.equal 'dev/stylesheet', _cssSeparation.getDirname 'dev/stylesheet/sample.scss'
 
 			return
 
+		return
+
 	describe '#createFile()', ->
 
 		it 'should create a file named "created.blank.file.css" in "dest" folder', ->
-
-			_cssSeparation = new cssSeparation()
 
 			_cssSeparation.createFile('', './dest/created.blank.file.css')
 
--- test/test.js
+++ test/test.js
@@ -1,43 +1,74 @@
 'use strict';
-var assert, cssSeparation, util;
+var assert, cssSeparation, glob, path, util, _cssSeparation;
 
 assert = require('assert');
+
+path = require('path');
+
+glob = require('glob');
 
 util = require('gulp-util');
 
 cssSeparation = require('../index');
 
+_cssSeparation = new cssSeparation({
+  mute: true,
+  conditionalClass: ['.ie6', '.ie7', '.ie8', '.ie9', '.ie10', '.ie11'],
+  beautify: true,
+  filterCommonCSS: true
+});
+
 describe('CSS-SEPARATION', function() {
-  describe('#getIdxListOfRuleContainCC()', function() {
-    return it('should return [4, 5, 6]', function() {
-      var idxsOfRuleContainCC, _cssSeparation, _rules;
-      _cssSeparation = new cssSeparation();
+  describe('#deal()', function() {
+    it('should create some file like "sample.ie6.css" around "sample.css" file', function() {
+      _cssSeparation.deal('test/sample.css');
+    });
+  });
+  describe('#getIdxListOfCommonCSS_AST()', function() {
+    it('should return [1, 2, 3]', function() {
+      var idxListOfCommonCSS, _rules;
       _rules = _cssSeparation.getRules_AST('test/sample.css');
-      idxsOfRuleContainCC = _cssSeparation.getIdxListOfRuleContainCC(_rules);
+      idxListOfCommonCSS = _cssSeparation.getPositionsOfSameClassOfRules('common', _rules);
+      assert.deepEqual([1, 2, 3], idxListOfCommonCSS);
+    });
+  });
+  describe('#getIdxListOfConditialCSS_AST()', function() {
+    it('should return [4, 5, 6]', function() {
+      var idxsOfRuleContainCC, _rules;
+      _rules = _cssSeparation.getRules_AST('test/sample.css');
+      idxsOfRuleContainCC = _cssSeparation.getPositionsOfSameClassOfRules('condition', _rules);
       assert.deepEqual([4, 5, 6], idxsOfRuleContainCC);
+    });
+  });
+  describe('#getIdxListOfMediaCSS_AST()', function() {
+    it('should return [7]', function() {
+      var idxListOfMediaCSS, _rules;
+      _rules = _cssSeparation.getRules_AST('test/sample.css');
+      idxListOfMediaCSS = _cssSeparation.getPositionsOfSameClassOfRules('media', _rules);
+      assert.deepEqual([7], idxListOfMediaCSS);
+    });
+  });
+  describe('#isContainConditionalSelector()', function() {
+    it('should return true if the given selectors list is [".ie7 .frmRegister > .fieldArea"]', function() {
+      assert.equal(true, _cssSeparation.isContainConditionalSelector([".ie7 .frmRegister > .fieldArea"]));
+    });
+    it('should return true if the given selectors list is [".ie7 .frmRegister > .iptPhoneCodeArea", ".ie8 .frmRegister > .iptPhoneCodeArea", ".ie9 .frmRegister > .iptPhoneCodeArea"]', function() {
+      assert.equal(true, _cssSeparation.isContainConditionalSelector([".ie7 .frmRegister > .iptPhoneCodeArea", ".ie8 .frmRegister > .iptPhoneCodeArea", ".ie9 .frmRegister > .iptPhoneCodeArea"]));
     });
   });
   describe('#getDirname()', function() {
     it('should return "./", if the given value is just "sample.css"', function() {
-      var _cssSeparation;
-      _cssSeparation = new cssSeparation();
       assert.equal('./', _cssSeparation.getDirname('sample.css'));
     });
     it('should return "./dest", if the given value is just "dest/sample.css"', function() {
-      var _cssSeparation;
-      _cssSeparation = new cssSeparation();
       assert.equal('./dest', _cssSeparation.getDirname('./dest/sample.css'));
     });
-    return it('should return "dev/stylesheet", if the given value is just "dev/stylesheet/sample.scss"', function() {
-      var _cssSeparation;
-      _cssSeparation = new cssSeparation();
+    it('should return "dev/stylesheet", if the given value is just "dev/stylesheet/sample.scss"', function() {
       assert.equal('dev/stylesheet', _cssSeparation.getDirname('dev/stylesheet/sample.scss'));
     });
   });
   describe('#createFile()', function() {
     it('should create a file named "created.blank.file.css" in "dest" folder', function() {
-      var _cssSeparation;
-      _cssSeparation = new cssSeparation();
       _cssSeparation.createFile('', './dest/created.blank.file.css');
     });
   });
